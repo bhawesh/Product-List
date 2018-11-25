62b8d485a2de09672d317e0d359ab00df6cee2c5
bhawesh <bhaweshkumarsharma1@gmail.com>
2018-11-26 00:27:52 +0530

    ngIf and ngFor

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
      pageTitle : string = 'Product List';
+     products :any= [
+       {
+     productId:1,
+     productName:'Hammer',
+     productCode:'123',
+     releaseDate: '26 Nov 2018',
+     price: '10$',
+     starRating: '3.5'
+    },
+    {
+      productId:2,
+      productName:'Face Wash',
+      productCode:'124',
+      releaseDate: '27 Nov 2018',
+      price: '10.5$',
+      starRating: '4.2'
+
+    }
+    
+    ];
   constructor() { }
 
   ngOnInit() {
