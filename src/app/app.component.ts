ae8d69f5d364f8e77443820e89151a4614f9c673
bhawesh <bhaweshkumarsharma1@gmail.com>
2018-11-25 22:28:31 +0530

    Binding with interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  Title : String = 'Acme Product-Management';
+ Title : String = 'Acme Product-Management';
 }
